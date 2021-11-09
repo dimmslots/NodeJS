6667f552d8cab051f136a99b663ead9739f8624b
Dimm <vctrline2048@gmail.com>
2021-11-09 20:52:13 +0700

    tutorial contact app NodeJS part III

--- contacts.js
+++ contacts.js
@@ -14,10 +14,17 @@ if (!fs.existsSync(filePath)) {
   fs.writeFileSync(filePath, '[]', 'utf-8');
 }
 
-const simpanContact = (nama, noHP) => {
-    const contact = { nama, noHP };
+const loadContact = () => {
     const file = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(file);
+    return contacts;
+}
+
+const simpanContact = (nama, noHP) => {
+    const contact = { nama, noHP };
+    // const file = fs.readFileSync('data/contacts.json', 'utf-8');
+    // const contacts = JSON.parse(file);
+    const contacts = loadContact();
   
     // cek duplikat
     const duplikat = contacts.find((contact) => contact.nama === nama);
@@ -36,6 +43,43 @@ const simpanContact = (nama, noHP) => {
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
   
     console.log('Terima kasih telah memasukkan data');
+};
+
+const listContact = () => {
+    const contacts = loadContact();
+    console.log('Daftar Contact');
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.noHP}`);
+    });
+};
+
+const detailContact = (nama) => {
+    const contacts = loadContact();
+
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase());
+
+    if(!contact) {
+        console.log(`${nama} tidak ada di daftar contact!`);
+        return false;
+    }
+
+    console.log('contact ditemukan!');
+    console.log(`Nama kontak : ${contact.nama}`);
+    console.log(`No HP kontak : ${contact.noHP}`);
 }
 
-module.exports = {simpanContact};
+const deleteContact = (nama) => {
+    const contacts = loadContact();
+    
+    const newContacts = contacts.filter((contact) => contact.nama.toLowerCase() !== nama.toLowerCase());
+
+    if(contacts.length === newContacts.length) {
+        console.log(`${nama} tidak ditemukan!`);
+        return false;
+    }
+
+    fs.writeFileSync('data/contacts.json', JSON.stringify(newContacts));
+    console.log(`data contact '${nama}' berhasil dihapus!`);
+};
+
+module.exports = {simpanContact, listContact, detailContact, deleteContact};
