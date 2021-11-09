1376443ad6106077b02bc6fc103fcbd3d5129601
Dimm <vctrline2048@gmail.com>
2021-11-09 22:07:49 +0700

    tutorial web server NodeJS

---
+++ app.js
@@ -0,0 +1,45 @@
+const { fstat } = require('fs');
+const http = require('http');
+const fs = require('fs');
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write('Error : File not found.');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    const url = req.url;
+    switch (url) {
+      case '/about':
+        renderHTML('./public/about.html', res);
+        break;
+
+      case '/contact':
+        renderHTML('./public/contact.html', res);
+        break;
+
+      default:
+        renderHTML('./public/index.html', res);
+        break;
+    }
+    // if (url === '/about') {
+    //   renderHTML('./public/about.html', res);
+    // } else if (url === '/contact') {
+    // } else {
+    //   renderHTML('./public/index.html', res);
+    // }
+  })
+  .listen(3000, () => {
+    console.log('Server is listening on port 3000...');
+    console.log('Check here! : http://localhost:3000');
+  });
