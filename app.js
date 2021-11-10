95a19c8059d412675322ef6a40cb5f9af9500ada
Dimm <vctrline2048@gmail.com>
2021-11-10 19:22:07 +0700

    tutorial web server ExpressJS - View Engine

--- app.js
+++ app.js
@@ -1,45 +1,96 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3000;
+const expressLayouts = require('express-ejs-layouts');
 
-const renderHTML = (path, res) => {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.write('Error : File not found.');
-    } else {
-      res.write(data);
-    }
-    res.end();
+// gunakan ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+  // res.sendFile('./public/index.html', { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: 'Dimas Adi P',
+      nim: '0074',
+    },
+    {
+      nama: 'Adi Dimas Q',
+      nim: '0088',
+    },
+    {
+      nama: 'Pangestu Dimas R',
+      nim: '0140',
+    },
+  ];
+  res.render('index', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman Home',
+    namepage: 'index.html',
+    mahasiswa,
   });
-};
+});
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-    });
-    const url = req.url;
-    switch (url) {
-      case '/about':
-        renderHTML('./public/about.html', res);
-        break;
-
-      case '/contact':
-        renderHTML('./public/contact.html', res);
-        break;
-
-      default:
-        renderHTML('./public/index.html', res);
-        break;
-    }
-    // if (url === '/about') {
-    //   renderHTML('./public/about.html', res);
-    // } else if (url === '/contact') {
-    // } else {
-    //   renderHTML('./public/index.html', res);
-    // }
-  })
-  .listen(3000, () => {
-    console.log('Server is listening on port 3000...');
-    console.log('Check here! : http://localhost:3000');
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman About',
+    namepage: 'about.html',
   });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman Contact',
+    namepage: 'contact.html',
+  });
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send('Page not found.');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// const fs = require('fs');
+
+// const renderHTML = (path, res) => {
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       res.writeHead(404);
+//       res.write('Error : File not found.');
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+//   });
+// };
+
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, {
+//       'Content-Type': 'text/html',
+//     });
+//     const url = req.url;
+//     switch (url) {
+//       case '/about':
+//         renderHTML('./public/about.html', res);
+//         break;
+
+//       case '/contact':
+//         renderHTML('./public/contact.html', res);
+//         break;
+
+//       default:
+//         renderHTML('./public/index.html', res);
+//         break;
+//     }
+//   })
+//   .listen(3000, () => {
+//     console.log('Server is listening on port 3000...');
+//     console.log('Check here! : http://localhost:3000');
+//   });
