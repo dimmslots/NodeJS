ffc2df8569cdbe05e15d26cdc7b4c4633af5b282
Dimm <vctrline2048@gmail.com>
2021-11-12 13:47:39 +0700

    Tutorial ExpressJS Contact App Part II

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const expressLayouts = require('express-ejs-layouts');
-const { loadContact, findContact } = require('./utils/contacts');
+const {
+  loadContact,
+  findContact,
+  addContact,
+  cekDuplikat,
+} = require('./utils/contacts');
+const { body, validationResult, check } = require('express-validator');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
 // gunakan ejs
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+
+// konfigurasi flash
+app.use(cookieParser('secret'));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 app.get('/', (req, res) => {
   // res.sendFile('./public/index.html', { root: __dirname });
@@ -47,17 +69,57 @@ app.get('/contact', (req, res) => {
     layout: 'layouts/main-layout',
     title: 'Halaman Contact',
     namepage: 'contact.html',
-    contacts: contacts
+    contacts: contacts,
+    msg: req.flash('msg'),
   });
 });
 
+// halaman form tambah data contact
+app.get('/contact/add', (req, res) => {
+  res.render('add-contact', {
+    title: 'Form Tambah Data Contact',
+    layout: 'layouts/main-layout',
+  });
+});
+
+// proses data contact
+app.post(
+  '/contact',
+  [
+    body('nama').custom((value) => {
+      const duplikat = cekDuplikat(value);
+      if (duplikat) {
+        throw new Error('Nama contact sudah ada!');
+      }
+      return true;
+    }),
+    check('noHP', 'Nomer HP tidak valid!').isMobilePhone('id-ID'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render('add-contact', {
+        title: 'Form Tambah Data Contact',
+        layout: 'layouts/main-layout',
+        errors: errors.array(),
+      });
+    } else {
+      addContact(req.body);
+      req.flash('msg', 'Data contact berhasil ditambahkan!');
+      res.redirect('/contact');
+    }
+  }
+);
+
+// halaman detail contact
 app.get('/contact/:nama', (req, res) => {
   const contact = findContact(req.params.nama);
 
   res.render('detail', {
     layout: 'layouts/main-layout',
     title: 'Halaman Detail Contact',
-    contact
+    contact,
   });
 });
 
